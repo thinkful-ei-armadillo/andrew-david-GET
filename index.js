5a93269734f6a1e25588a4532054f40aafddb09c
David Haugen <haugen.dj@gmail.com>
2019-01-25 12:29:25 -0600

    add form for search function

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 'use strict';
 
+/* global $ */
+
 function handleGetDog(){
-    $('.js-form').on('submit', function(event){
-        event.preventDefault();
-        const inputNum = $('.js-user-input').val();
-        validateInputNum(inputNum);
-        getDogImages(inputNum);
-        // console.log(`getDogImages is :${getDogImages(inputNum)}`);
-        // displayDogImages(getDogImages(inputNum));
+  $('.js-form').on('submit', function(event){
+    event.preventDefault();
+    const inputNum = $('.js-user-input').val();
+    validateInputNum(inputNum);
+    getDogImages(inputNum);
+    // console.log(`getDogImages is :${getDogImages(inputNum)}`);
+    // displayDogImages(getDogImages(inputNum));
   });
 }
 
 function validateInputNum(input){
-    if (input < 1 || input > 50){
-        $('.image-container').html('<p>Please select a number between 1 and 50.</p>');
-        throw new Error();
-    }
+  if (input < 1 || input > 50){
+    $('.image-container').html('<p>Please select a number between 1 and 50.</p>');
+    throw new Error();
+  }
 }
 
 function getDogImages(input){
-    fetch(`https://dog.ceo/api/breeds/image/random/${input.toString()}`)
+  fetch(`https://dog.ceo/api/breeds/image/random/${input.toString()}`)
     .then(response => response.json())
     .then(data => {
-        const imageArr = data.message;
-        console.log(imageArr);
-        displayDogImages(imageArr);
+      const imageArr = data.message;
+      console.log(imageArr);
+      displayDogImages(imageArr);
     });
-    
 }
 
 function displayDogImages(data){
-    console.log(data);
-    $('.image-container').html(data.map(element => `<img src="${element}">`));
+  console.log(data);
+  $('.image-container').html(data.map(element => `<img src="${element}">`));
+}
+
+function handleSearchDog(){
+  $('.js-search-form').on('submit', function(event){
+    event.preventDefault();
+    const dogBreed = $('.js-breed-search').val();
+  });
 }
 
 function main(){
   handleGetDog();
+  handleSearchDog();
 }
 
 $(main);
