65d7e74359ac0046f1b4512c21a04124974d0343
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-25 09:56:08 -0800

    added dog api app working up to checkpoint number 2

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function handleGetDog(){
+    $('.js-form').on('submit', function(event){
+        event.preventDefault();
+        const inputNum = $('.js-user-input').val();
+        validateInputNum(inputNum);
+        getDogImages(inputNum);
+        // console.log(`getDogImages is :${getDogImages(inputNum)}`);
+        // displayDogImages(getDogImages(inputNum));
+  });
+}
+
+function validateInputNum(input){
+    if (input < 1 || input > 50){
+        $('.image-container').html('<p>Please select a number between 1 and 50.</p>');
+        throw new Error();
+    }
+}
+
+function getDogImages(input){
+    fetch(`https://dog.ceo/api/breeds/image/random/${input.toString()}`)
+    .then(response => response.json())
+    .then(data => {
+        const imageArr = data.message;
+        console.log(imageArr);
+        displayDogImages(imageArr);
+    });
+    
+}
+
+function displayDogImages(data){
+    console.log(data);
+    $('.image-container').html(data.map(element => `<img src="${element}">`));
+}
+
+function main(){
+  handleGetDog();
+}
+
+$(main);
